12515def51441818662d7ede5e1700e961527396
13248519492a <2271035216@qq.com>
2026-07-28 16:04:40 +0800

    fix ci path

--- firmware/IotDesk/components/protocols/wifi.c
+++ firmware/IotDesk/components/protocols/wifi.c
@@ -4,7 +4,8 @@
 
 #define TAG "WIFI"
 
-void wifi_init(void)
+void wifi_sta_init(void)
 {
-    
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 }
--- firmware/IotDesk/components/protocols/wifi.h
+++ firmware/IotDesk/components/protocols/wifi.h
@@ -1,3 +1,6 @@
 #ifndef PROTOCOLS_WIFI_H
 #define PROTOCOLS_WIFI_H
+
+void wifi_sta_init(void);
+
 #endif
